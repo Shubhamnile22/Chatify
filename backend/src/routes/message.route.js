fcc666a35c69c8bb061cca9e83533e2b21c3ff64
Shubhamnile22 <theshubhamnile@gmail.com>
2025-09-22 12:31:46 +0530

    message routes updated

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import { protectRoute } from '../middleware/auth.middleware.js';
+import { arcjetProtection } from '../middleware/arcjet.middleware.js';
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from '../controllers/message.controller.js';
 
 const router = express.Router();
 
-router.get('/send', (req, res) => {
-  res.send('Hello from send message route');
-});
+router.use(arcjetProtection, protectRoute);
+
+router.get('/contacts', getAllContacts);
+router.get('/chats', getChatPartners);
+router.get('/:id', getMessagesByUserId);
+router.post('/send/:id', sendMessage);
 
 export default router;
